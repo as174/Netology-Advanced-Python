830d712b57f9359c076766a2a5b45e00116bfdd8
as174 <42006639+as174@users.noreply.github.com>
2019-05-30 13:43:46 +0300

    Create main.py

---
+++ HW-1 Import.Module.Package/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 13:26:55 2019
+
+@author: RU20009341
+"""
+
+from application import salary, db
+from application.db import people
+
+if __name__ == '__main__':
+    get_employees()
+    calculate_salary()
