e920fcf659f33c0983c1b73654e75414c0fd31bc
as174 <42006639+as174@users.noreply.github.com>
2019-06-05 23:34:47 +0300

    Update HW-2.py

--- HW-2 Iterators.Generators.Yield/HW-2.py
+++ HW-2 Iterators.Generators.Yield/HW-2.py
@@ -7,11 +7,10 @@ Created on Tue Jun  4 21:56:53 2019
 
 #Написать класс итератора, который по каждой стране из файла countries.json ищет страницу из википедии.
 #Записывает в файл пару: страна – ссылка.
-#
-#Написать генератор, который принимает путь к файлу. 
-#При каждой итерации возвращает md5 хеш каждой строки файла.
+
 
 import json
+import hashlib
 
 class Country_iterator:
     
@@ -28,10 +27,29 @@ class Country_iterator:
         if self.counter < len(self.data):
             country_name = self.data[self.counter]['name']['common']
             pair = country_name + ' - ' + 'https://en.wikipedia.org/wiki/'+country_name
-            with open ('countries.csv', 'a', encoding = 'utf-8') as file:
+            with open ('countries.txt', 'a', encoding = 'utf-8') as file:
                 file.writelines(pair + '\n')
         else:
             raise StopIteration
+            
 country_list = Country_iterator('countries.json')
+
 for item in country_list:
     next(country_list)
+
+#Написать генератор, который принимает путь к файлу. 
+#При каждой итерации возвращает md5 хеш каждой строки файла.      
+    
+    
+def md5_generator(path):
+    with open(path, 'r', encoding = 'utf-8') as file:
+        while True:
+            try:
+                yield hashlib.md5(file.readline().splitlines()[0].encode()).hexdigest()
+            except IndexError:
+                break
+
+for i in md5_generator('countries.txt'):
+    print(i)
+
+
